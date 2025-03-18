a8afc9f9db21b14993bdd5482e7fe5456f253db0
grantmerrigan <gmerrigan13@gmail.com>
2025-03-18 12:20:02 -0400

    Added saving and restoring of die roll state

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -12,6 +12,10 @@ class DieFragment : Fragment() {
 
     val DIESIDE = "sidenumber"
 
+    val PREVIOUS_ROLL = "previousroll"
+
+    var currentRoll = 0
+
     lateinit var dieTextView: TextView
 
     var dieSides: Int = 6
@@ -35,15 +39,27 @@ class DieFragment : Fragment() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(PREVIOUS_ROLL, currentRoll)
+    }
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
-        view.setOnClickListener{
+        if (savedInstanceState == null) {
             throwDie()
+        } else {
+            currentRoll = savedInstanceState.getInt(PREVIOUS_ROLL)
+            dieTextView.text = currentRoll.toString()
         }
     }
 
     fun throwDie() {
-        dieTextView.text = (Random.nextInt(dieSides)+1).toString()
+        currentRoll = Random.nextInt(dieSides) + 1
+        dieTextView.text = currentRoll.toString()
     }
+
+
 }
